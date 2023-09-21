b5ba5cb29ffb48b937ec1816b0484ff94b003abb
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-09-21 10:40:44 +0200

    init repo

---
+++ C/2023/KW38/NumberGuesser2/main.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:b5f9873d91b5ff05008b3bf111ab3aae09efbaab6f35ed0aaea9debfc8466112
+size 1097
--- C/2023/KW38/SchaltjahrRechner/main.c
+++ C/2023/KW38/SchaltjahrRechner/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:581c2a95caa20112c07680fbd2e07b5e6c0282276c90f158ceed6974faaeb58f
-size 1308
+oid sha256:075b5172987acabeea9e799f0b5051c32a077bae04c6be149cdc6ead05cccf90
+size 1331
