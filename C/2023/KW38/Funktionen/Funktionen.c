858d616be22215e0867032cbe8a8f9ce496c4acd
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-09-21 08:34:45 +0200

    update

---
+++ C/2023/KW38/Funktionen/Funktionen.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:9d617b6a1c20226b929919be6f1065dc005750ed2c805f15bdf321a40751de63
+size 3136
--- C/2023/KW38/SchaltjahrRechner/main.c
+++ C/2023/KW38/SchaltjahrRechner/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:57e12b0bcef12d68bd4bc705df360f237b2868bf6acf930bd150b4d3588f011d
-size 1262
+oid sha256:581c2a95caa20112c07680fbd2e07b5e6c0282276c90f158ceed6974faaeb58f
+size 1308
