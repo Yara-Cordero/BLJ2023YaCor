4e84522bc7164b86cf2d16e702ab823bcbdc4bc4
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-09-28 16:27:53 +0200

    finished the mark calculator (minimum)

--- C/2023/KW39/MarkCalculator/main.c
+++ C/2023/KW39/MarkCalculator/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:9686c47a80af76c4bc0f04faf702db46106d8f0cce6fd4e9bd82a097106d5598
-size 976
+oid sha256:bec18c95b55ad42b84d75e05a1752d7ffe7a53094621ff7d92dc048112f5d4ca
+size 1183
