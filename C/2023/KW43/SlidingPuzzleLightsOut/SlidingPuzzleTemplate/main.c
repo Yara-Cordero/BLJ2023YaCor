9c3e8e91a5eb5c738137696a763a6d6be927d470
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-10-26 16:22:28 +0200

    added Lights Out Template

---
+++ C/2023/KW43/SlidingPuzzleLightsOut/SlidingPuzzleTemplate/main.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:733c36ae43198850de7bb118aef8693810a716e3a4a2b886fddd62519b5bb1e2
+size 1184
---
+++ C/2023/KW43/SlidingPuzzleLightsOut/SlidingPuzzleTemplate/utils.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:ed854d2cd66209eb88cea56cf5f9faaa781ad3e402012d530d0ab7fc2e726084
+size 135
