83d6af5f116d70908cede3e59cf8da4554aab280
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-01 15:56:52 +0100

    base expectations for Personen Manager

---
+++ C/2023/KW44/PersonenManager/family.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:305df7e8da87ccfc01aaf0cb1791f4fb24f9ac16a4f4510b1081a9be9ed094c0
+size 2144
---
+++ C/2023/KW44/PersonenManager/main.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8074fd2815cac08c06015a0c6758b0ccdacbd9a21e0aa9af12f67afbd162a837
+size 4256
