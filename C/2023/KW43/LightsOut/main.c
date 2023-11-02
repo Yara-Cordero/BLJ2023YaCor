d450800172fc65952fe4c72d57ad4994fff54892
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-02 08:50:00 +0100

    finished Lights out

--- C/2023/KW43/LightsOut/main.c
+++ C/2023/KW43/LightsOut/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6eb14e86b773c1a85df5465292e1e4d5e7bbc8ae74b2fbc15326254f00dd447f
-size 8562
+oid sha256:b06b65d912ce334010f877f58aad65083aadd49c8282ef36810a33e7405ef79f
+size 10049
