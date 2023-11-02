4689a43115829925d8b2ec4e72ede3a568fbff4b
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-02 08:56:28 +0100

    deleted useless file

--- C/2023/KW44/PersonenManager/main.c
+++ C/2023/KW44/PersonenManager/main.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8074fd2815cac08c06015a0c6758b0ccdacbd9a21e0aa9af12f67afbd162a837
-size 4256
+oid sha256:97da86e446442ef633978df0ae4b843e9a5abe226327fcddd0de13cbead41f95
+size 4886
