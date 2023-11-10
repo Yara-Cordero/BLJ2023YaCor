03d57705c0369d6c43bf2ed345bc97f555648170
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-10 11:50:33 +0100

    malloc() versions made

---
+++ C/2023/KW44/Bobby input/AnikMalloc.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:59cb8e913986dd90ca4d74fd171097d8e6036f5134257ab10366e38642a5763a
+size 450
---
+++ C/2023/KW44/Bobby input/LeonaMalloc.c
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e33714b1bdc3cf162b355c6e33fced250f4d668a7ebae8c8119c7d79a753c864
+size 457
