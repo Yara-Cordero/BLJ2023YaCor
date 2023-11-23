ca054dda0d7cbed62c017ee9016def97acb237d3
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-23 16:11:40 +0100

    finished PIN-Code-Generator

---
+++ Java/2023/KW47/PIN-Code-Generator/src/PINCodeGenerator/Main.java
@@ -0,0 +1,17 @@
+package PINCodeGenerator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to the PIN-Code-Generator!");
+
+        int digit = 0;
+        do{
+            String pin = String.format("%04d", digit);
+            System.out.println(pin);
+            digit++;
+        }while(digit < 10000);
+
+
+    }
+}
