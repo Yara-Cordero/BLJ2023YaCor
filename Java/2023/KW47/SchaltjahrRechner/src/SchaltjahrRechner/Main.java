a8c88b9f7ecd260d85f021307385613bf7c61c59
Yara-Cordero <yara.cordero@noseryoung.ch>
2023-11-24 09:34:26 +0100

    finished leap year calculator

---
+++ Java/2023/KW47/SchaltjahrRechner/src/SchaltjahrRechner/Main.java
@@ -0,0 +1,37 @@
+package SchaltjahrRechner;
+import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+
+public class Main {
+    private static void slowPrint(String output) {
+        for (int i = 0; i<output.length(); i++) {
+            char c = output.charAt(i);
+            System.out.print(c);
+            try {
+                TimeUnit.MILLISECONDS.sleep(25);
+            }
+            catch (Exception e) {
+
+            }
+        }
+    }
+    private static boolean isLeapYear (int year) {
+        return (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Welcome to the leap year calculator!");
+
+        System.out.println("Enter a year: ");
+        int year = scan.nextInt();
+
+        if(isLeapYear(year)) {
+            System.out.println("The year " + year + " is a leap year!!");
+        }else {
+            slowPrint("The year " + year + " is NOT a leap year...");
+        }
+
+
+    }
+}
