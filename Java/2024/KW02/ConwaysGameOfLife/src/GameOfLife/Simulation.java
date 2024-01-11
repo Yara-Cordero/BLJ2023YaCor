153691d825bbf3bb5f7960366a4f8504889a4b3f
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-01-11 11:05:02 +0100

    completed basic functions for game of life

--- Java/2024/KW02/ConwaysGameOfLife/src/GameOfLife/Simulation.java
+++ Java/2024/KW02/ConwaysGameOfLife/src/GameOfLife/Simulation.java
@@ -2,11 +2,12 @@ package GameOfLife;
 
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
+import java.util.Arrays;
 
 /**
  * This class is responsible for handling and running a simulation of Conway's
  * Game of Life.
- * 
+ *
  * @author surber
  *
  */
@@ -20,7 +21,7 @@ public class Simulation extends Thread {
 
   /**
    * A constructor.
-   * 
+   *
    * @param speed     An int value that controls the speed of the simulation.
    *                  Lower value equals faster simulation speed.
    * @param wrapField A boolean value that toggles whether the simulation field is
@@ -36,7 +37,7 @@ public class Simulation extends Thread {
 
   /**
    * A constructor.
-   * 
+   *
    * @param speed     An int value that controls the speed of the simulation.
    *                  Lower value equals faster simulation speed.
    * @param wrapField A boolean value that toggles whether the simulation field is
@@ -69,35 +70,15 @@ public class Simulation extends Thread {
   /**
    * This method is used for running the simulation.
    */
-  private void startSimulation() {
+  private void startSimulation() {    //The Simulation runs as long as the 'running' variable is true
     while (running) {
 
       /*
        * Changes from the current to the next generation get written into a new array,
-       * instead of directly overwriting the old one. Why is that?
+       * instead of directly overwriting the old one. Why ist that?
        */
-      int[][] newField = new int[field.length][field[0].length];
+      int[][] newField = new int[field.length][field[0].length];    //Create a new 2D Array to hold the next generation
 
-      /*
-      int width = getFieldWidth();
-      int height = getFieldHeight();
-       */
-
-      for(int i = 0; i < field.length; i++ ){
-        for(int j = 0; j < field[i].length; j++){
-            int neighbourCount = getNeighbourCount(i, j);
-
-            if(field[i][j] == 1){
-              if(neighbourCount < 2|| neighbourCount > 3){
-                field[i][j] = 0;
-              }
-            }else {
-              if(neighbourCount == 3){
-                field[i][j] = 1;
-              }
-            }
-        }
-      }
       /*
        * TODO: Implement the main logic used for running the simulation. Make sure to
        * not implement everything here, but instead use the methods below (also to be
@@ -105,14 +86,49 @@ public class Simulation extends Thread {
        * field, which allows you to look at the values contained in each cell.
        */
 
+      //Iteration over each cell in the current field
+      for (int i = 0; i < field.length; i++) {
+        for (int j = 0; j < field[i].length; j++) {
+          //Get the count of live neighbours for the current cell
+          int neighbours = getNeighbourCount(i, j);
+
+          //If the current cell is alive
+          if (field[i][j]==1){
+            //If the cell has ferwer than 2 or more than 3 live neighbours, it dies
+            if (neighbours < 2 || neighbours > 3){
+              newField[i][j] = 0;
+            }
+            //Otherwise, the cell survives
+            else {
+              newField[i][j] = 1;
+            }
+          }
+          //If the current cell is dead
+          else {
+            //If the cell has exactly 3 live neighbours, it comes to life
+            if (neighbours == 3){
+              newField[i][j] = 1;
+            }
+          }
+        }
+      }
+      //If the current and next generation are the same, stop the simulation
+      if (Arrays.deepEquals(field, newField)){       //Arrays.deepEqual checks if two arrays are deeply equal or not
+        stopSimulation();
+      }
       /*
        * TODO: Stop the simulation if the current and next generation are the same. In
        * order to do that, call the method stopSimulation() under the right
        * circumstances.
        */
 
+      //Notify any listeners of the change in the field
       pCS.firePropertyChange("field", field, newField);
+
+      //replace the current field with the new one
       field = newField;
+
+      //Pause the simulation for 'speed' milliseconds
       try {
         Thread.sleep(speed);
       } catch (InterruptedException e) {}
@@ -128,7 +144,7 @@ public class Simulation extends Thread {
    * N|C|N <br>
    * -+-+- <br>
    * N|N|N <br>
-   * 
+   *
    * @param x The x-coordinate of a cell.
    * @param y The y-coordinate of a cell.
    * @return An int value representing the number of neighbours of a cell.
@@ -146,45 +162,46 @@ public class Simulation extends Thread {
           }
         }
       }
-    }
 
-    /*
-    if(field[x-1][y-1] == 1){
-      neighbourCount++;
-    }
-    if(field[x-1][y] == 1){
-      neighbourCount++;
-    }
-    if (field[x][y-1] == 1){
-      neighbourCount++;
-    }
-    if (field[x][y+1] == 1){
-      neighbourCount++;
-    }
-    if (field[x+1][y-1] == 1){
-      neighbourCount++;
-    }
-    if (field[x+1][y] == 1){
-      neighbourCount++;
-    }
-    if (field[x+1][y+1] == 1){
-      neighbourCount++;
-    }
-     */
-
+        /*if(field[x-1][y-1]==1){
+          neighbourCount++;
+        }
+        if(field[x-1][y]==1){
+          neighbourCount++;
+        }
+        if(field[x-1][y+1]==1){
+          neighbourCount++;
+        }
+        if(field[x][y-1]==1){
+          neighbourCount++;
+        }
+        if(field[x][y+1]==1){
+          neighbourCount++;
+        }
+        if(field[x+1][y-1]==1){
+          neighbourCount++;
+        }
+        if(field[x+1][y]==1){
+          neighbourCount++;
+        }
+        if(field[x+1][y+1]==1){
+          neighbourCount++;
+        }
+        *\
     /*
      * TODO: Implement logic that counts the neighbours of a given cell. Neighbours
      * are up to 8 living cells next to the cell in question, according to the
      * comment above.
      */
-
+    }
     return neighbourCount;
   }
+
 
   /**
    * This helper method is used to check whether the coordinates of a cell are
    * valid i.e. the coordinates are in bounds of the simulation field.
-   * 
+   *
    * @param x The x-coordinate of a cell to check.
    * @param y The y-coordinate of a cell to check.
    * @return A boolean value representing the fact whether a cell's coordinates
@@ -192,20 +209,31 @@ public class Simulation extends Thread {
    */
   private boolean coordsInBounds(int x, int y) {
 
-    /*
-     * TODO: Implement logic that checks if the given coordinates are in bounds,
-     * i.e. neither the x or y coordinate is negative or greater than the width or
-     * height of the simulation field.
-     */
+    if (x >= 0 && x < field.length) {
+      if (y >= 0 && y < field[0].length) {
+        return true;
+      } else {
+        return false;
+      }
 
-    return false;
+    } else {
+
+
+      /*
+       * TODO: Implement logic that checks if the given coordinates are in bounds,
+       * i.e. neither the x or y coordinate is negative or greater than the width or
+       * height of the simulation field.
+       */
+
+      return false;
+    }
   }
 
   /**
    * This helper method is used to count additional neighbours in the case of a
    * wrapped simulation field. It is only relevant for cells that are positioned
    * on the edges (including corners) of the simulation field.
-   * 
+   *
    * @param x The x-coordinate of a cell.
    * @param y The y-coordinate of a cell.
    * @return An int value representing the number of wrapped neighbours of a cell.
@@ -216,7 +244,7 @@ public class Simulation extends Thread {
      * TODO: INTERMEDIATE TASK - The simulation field is limited in size and has a
      * border. Having the simulation field wrap around itself means that something
      * can disappear at the right border and reappear on the left. Same for top and
-     * bottom and in all directions. 
+     * bottom and in all directions.
      */
 
     return 0;
@@ -225,7 +253,7 @@ public class Simulation extends Thread {
   /**
    * This helper method is used to prolong the initial state of the simulation
    * before continuing with the given simulation speed.
-   * 
+   *
    * @param timeout The amount of time (in milliseconds) to hold the initial
    *                simulation state.
    */
@@ -239,7 +267,7 @@ public class Simulation extends Thread {
   /**
    * This method is used to add a {@link PropertyChangeListener} to the
    * {@link PropertyChangeSupport} object of this class.
-   * 
+   *
    * @param pCL The PropertyChangeListener object to add.
    */
   public void addListener(PropertyChangeListener pCL) {
--- Java/2024/KW02/ConwaysGameOfLife/src/GameOfLife/Starter.java
+++ Java/2024/KW02/ConwaysGameOfLife/src/GameOfLife/Starter.java
@@ -8,7 +8,7 @@ package GameOfLife;
  */
 public class Starter {
   public static void main(String[] args) {
-    Simulation sim = new Simulation(100, false, Preset.DEMO_10X10);
+    Simulation sim = new Simulation(200, false, Preset.SPACESHIPS_20X20);
     SimulationPanel simPanel = new SimulationPanel(sim, 40, 0.9);
     simPanel.getSimulation().start();
   }
