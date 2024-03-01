ddcd74864a683c79f0bcef3fc2a5f8bf625c7b26
Yara-Cordero <145566014+Yara-Cordero@users.noreply.github.com>
2024-03-01 15:48:06 +0100

    finished part one of assignment

---
+++ Java/2024/KW09/Cat-Quote/Cat-Quote/src/CatQuote/Main.java
@@ -0,0 +1,35 @@
+package CatQuote;
+
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    static boolean isRunning;
+    private static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        ZenQuote Quote = new ZenQuote();
+        boolean error;
+
+        do {
+            Quote.getQuote();
+            do {
+                System.out.println("\n<m> more Quotes");
+                System.out.println("<q> quit");
+
+                String answer = scan.nextLine().toLowerCase().trim().replaceAll("\\s+", " ");
+
+                if(answer.equals("m")){
+                    isRunning = true;
+                    error = false;
+                } else if (answer.equals("q")) {
+                    isRunning = false;
+                    error = false;
+                } else {
+                    error = true;
+                }
+            }while (error);
+        }while (isRunning);
+    }
+}
---
+++ Java/2024/KW09/Cat-Quote/Cat-Quote/src/CatQuote/ZenQuote.java
@@ -0,0 +1,55 @@
+package CatQuote;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ZenQuote {
+
+    public ZenQuote() {
+    }
+
+    private static String getValue(String json, String key){
+        int startIndex = json.indexOf("\"" + key + "\":") + key.length() + 4;
+        int endIndex = json.indexOf("\"", startIndex);
+        return json.substring(startIndex, endIndex);
+    }
+
+    public void getQuote() throws IOException {
+        URL url = new URL("https://zenquotes.io/api/random");
+
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        connection.setRequestMethod("GET");
+
+        int responseCode = connection.getResponseCode();
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            StringBuilder response = new StringBuilder();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            String jsonString = response.toString();
+
+            int startIndex = jsonString.indexOf("{");
+            int endIndex = jsonString.lastIndexOf("}") + 1;
+            String json = jsonString.substring(startIndex, endIndex);
+
+
+            String quoteText = getValue(json, "q");
+            String author = getValue(json, "a");
+
+            System.out.println("Quote: " + quoteText);
+            System.out.println("Author: " + author);
+        } else {
+            System.out.println("Error: Unable to fetch quotes. Response code: " + responseCode);
+        }
+    }
+}
