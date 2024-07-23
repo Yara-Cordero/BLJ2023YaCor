9f0b59766720adf239a4a5f9105da854da4e5d5a
ncavegn <naica922@gmail.com>
2024-07-23 10:28:12 +0200

    Create drivers page with content and styling

--- UEK/uek-294/APIWebsite/f1-now/src/service/DriverService.ts
+++ UEK/uek-294/APIWebsite/f1-now/src/service/DriverService.ts
@@ -2,16 +2,16 @@ import { AxiosInstance } from "axios";
 import { defaultAxiosInstance } from "./Api";
 
 const DriverService = (api: AxiosInstance = defaultAxiosInstance) => ({
-    getAllCurrentDrivers: async () => {
-        const response = await api.get(`/f1/current/drivers.json`);
-        console.log(response);
-        return response.data;
-    },
-    getDriverByName : async (name : string) => {
-        const response = await api.get(`/f1/drivers/${name}.json`);
-        console.log(response);
-        return response.data;
-    }
-})
+  getAllCurrentDrivers: async () => {
+    const response = await api.get(`/f1/current/drivers.json`);
+    console.log("Raw API Response:", response);
+    return response.data;
+  },
+  getDriverByName: async (name: string) => {
+    const response = await api.get(`/f1/drivers/${name}.json`);
+    console.log("Raw API Response:", response);
+    return response.data;
+  },
+});
 
-export default DriverService
+export default DriverService;
